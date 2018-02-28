fix incorrect calculation of loss

cnn.py
@@ -36,7 +36,6 @@ def cnn_fn(features, labels, mode, params):
 
     # Calculate loss using mean squared error
     mean_squared_error = tf.losses.mean_squared_error(labels=labels, predictions=predictions)
-    root_mean_squared_error = tf.sqrt(mean_squared_error)
 
     # # Pre-made estimators use the total_loss instead of the average,
     # # so report total_loss for compatibility.
@@ -50,34 +49,18 @@ def cnn_fn(features, labels, mode, params):
             loss=mean_squared_error, global_step=tf.train.get_global_step())
 
         return tf.estimator.EstimatorSpec(
-            mode=mode, loss=root_mean_squared_error, train_op=train_op)
+            mode=mode, loss=mean_squared_error, train_op=train_op)
 
     # In evaluation mode we will calculate evaluation metrics.
     assert mode == tf.estimator.ModeKeys.EVAL
 
     eval_metrics = {}
 
-    # TODO: There's nothing to do. I just want to highlight what total bullshit 'tf.metrics.root_mean_squared_error' is.
-    # TODO: It's not RMSE for the predictions of just this batch, it's a rolling RMSE over the whole session.
-    # TODO: Meaning it's never what you expect it to be.
-    # TODO: tf.metrics.root_mean_squared_error is a lie!
-    # TODO: It used to be called 'streaming_mean_squared_error' but then someone decided "No, let's confuse everyone."
-
-    # rmse = tf.metrics.root_mean_squared_error(labels, predictions)
-    # eval_metrics["wish_i_knew_what_this_did_2_hours_ago_rmse"] = rmse
+    rmse = tf.metrics.root_mean_squared_error(labels, predictions)
+    eval_metrics["validation_rmse"] = rmse
 
     return tf.estimator.EstimatorSpec(
         mode=mode,
-        loss=root_mean_squared_error,
+        loss=mean_squared_error,
         eval_metric_ops=eval_metrics)
 
-    # TODO: Furthermore, see that loss parameter in the estimator spec right there? Yeah it's a lie too,
-    # TODO: but it's a lie for the good of society. If you pass an RMSE tensor like that in training mode,
-    # TODO: it'll report what you gave, the RMSE for the batch, but when you're evaluating, you don't want that.
-    # TODO: When evaluating, you want the RMSE over ALL the batches, and guess what?
-    # TODO: In the Estimator evaluate() function,
-    # TODO: they average that loss over all the batches before reporting it back to you,
-    # TODO: so you don't have to worry about it, that is unless you question the meaning of your own existence and
-    # TODO: realize that none of the loss functions are doing explicitly what you'd think they should do and then time itself starts to become an incorrect implementation of RMSE and you can see the RMSE between your past and future selves
-
-    # TODO: I promise, 'loss' on tensorboard is RMSE. It works, I swear. - JB <3